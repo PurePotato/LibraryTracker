415f00aab49a48b4bc608b86672416d93de51b06
Catie <catiebug324@gmail.com>
2023-03-12 17:02:22 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import List from "./components/List";
+//import Header from "./components/Header";
+//import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      {/*<Header /> */}
+      <List />
+      {/*<Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,146 @@
+import React from "react";
+export default class List extends React.Component {
+  
+  
+
+  changeHandler = (event) => {
+    const nam = event.target.name;
+    const val = event.target.value;
+    this.setState({
+      [nam]: val,
+    });
+  };
+
+  removeBook = (index) => {
+    const booksArr = [...this.state.books];
+    if (booksArr) {
+      this.setState(
+        {
+          books: booksArr.filter((book, bookIndex) => {
+            return bookIndex !== index;
+          }),
+        },
+        () => {
+          localStorage.setItem("books", JSON.stringify(this.state.books));
+        }
+      );
+    }
+  };
+
+  saveLocal = () => {
+    localStorage.setItem('books', JSON.stringify(this.state.books));
+  };
+
+  componentDidMount() {
+    const books = localStorage.getItem("books");
+    if (books) this.setState({ books: JSON.parse(books) });
+  };
+
+  submitHandler = (event) => {
+  event.preventDefault();
+  const bookNameVal = this.state.bookName;
+  const bookAuthorVal = this.state.bookAuthor;
+  const readVal = this.state.read;
+  if (bookNameVal && bookAuthorVal) {
+    this.setState(
+      (prevState) => ({
+        books: [
+          ...prevState.books,
+          {
+            bookName: bookNameVal,
+            bookAuthor: bookAuthorVal,
+            read: readVal,
+          },
+        ],
+      }),
+      () => {
+        localStorage.setItem("books", JSON.stringify(this.state.books));
+      }
+    );
+  }
+};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        bookAuthor: "",
+        bookName: "",
+        read: "No",
+        books: [],
+    };
+    
+  }  render() {
+    let books = this.state.books;
+    return (
+      <div>
+      <form className="bookForm" onSubmit={this.submitHandler}>
+          <label for="bookName">Book Title</label>
+          <input
+            id="bookName"
+            name="bookName"
+            type="text"
+            placeholder="Book Title"
+            maxLength="40"
+            onChange={this.changeHandler}
+            required
+           />
+          <label for="bookAuthor">Author</label>
+          <input
+            id="bookAuthor"
+            name="bookAuthor"
+            type="text"
+            placeholder="Book Author"
+            maxLength="30"
+            onChange={this.changeHandler}
+            required
+          />
+          <label for="read">Read</label>
+          <select
+            id="read"
+            name="read"
+            onChange={this.changeHandler}
+            value={this.state.read}
+          >
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+          </select>
+          <input id="submit" type="submit" value="ADD NEW
+BOOK" />
+</form>
+<table>
+          <tr>
+            <th>Book Name</th>
+            <th>Book Author</th>
+            <th>Finished (Yes/No)</th>
+            <th colSpan="2">Settings</th>
+          </tr></table>
+          <table>
+{books.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.bookName}</td>
+                <td>{item.bookAuthor}</td>
+                <td>{item.read}</td>
+                <td id="settings">
+                  <button
+                    onClick={() => {
+                      item.read === "Yes"
+                        ? (item.read = "No")
+                        : (item.read = "Yes");
+                        this.saveLocal();
+                        this.forceUpdate();
+                    }}
+                  >
+                    {item.read === "Yes" ? "Still reading" : "Finished"}
+                  </button>
+                  <button
+                    onClick={() => {
+                      this.removeBook(index);
+                    }}>Remove</button></td>
+              </tr>
+            );})}
+            </table>
+      </div>
+    );
+  }
+}
